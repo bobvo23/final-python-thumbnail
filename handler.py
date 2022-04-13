1733313b86a1d1cfef04588122c17c090622cd82
Bob Vo <vo.tri.thong@gmail.com>
2022-04-13 14:28:13 -0400

    Fuctional v2. Add url and meta data to DynamoDB

--- handler.py
+++ handler.py
@@ -7,8 +7,12 @@ import os
 import uuid
 import json
 
+
 s3 = boto3.client('s3')
 size = int(os.environ['THUMBNAIL_SIZE'])
+dbtable = str(os.environ['DYNAMODB_TABLE'])
+dynamodb = boto3.resource(
+    'dynamodb', region_name=str(os.environ['REGION_NAME']))
 
 
 def image_to_thumbnail(image):
@@ -35,8 +39,28 @@ def upload_to_s3(bucket, key, image, img_size):
     print(response)
 
     url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
-
+    dynamo_log = s3_save_thumbnail_url_to_dynamo(
+        url_path=url, img_size=img_size)
+    print(dynamo_log)
     return url
+
+
+def s3_save_thumbnail_url_to_dynamo(url_path, img_size):
+    toint = float(img_size*0.53)/1000
+    table = dynamodb.Table(dbtable)
+    response = table.put_item(
+        Item={
+            'id': str(uuid.uuid4()),
+            'url': str(url_path),
+            'approxReducedSize': str(toint) + str(' KB'),
+            'createdAt': str(datetime.now()),
+            'updatedAt': str(datetime.now())
+        }
+    )
+    return {
+        "statusCode": 200,
+        'headers': {'Content-Type': 'application/json'},
+        "body": json.dumps(response)}
 
 
 def s3_thumbnail_generator(event, context):
